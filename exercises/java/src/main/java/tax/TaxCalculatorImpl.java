c0927518292c69ada7cd9a43f761b7eb7d883951
sztanderap <sztandera.p@gmail.com>
2020-01-20 12:13:01 +0000

    add new TaxCalculatorImpl

--- exercises/java/src/main/java/tax/TaxCalculatorImpl.java
+++ exercises/java/src/main/java/tax/TaxCalculatorImpl.java
@@ -10,33 +10,33 @@ public class TaxCalculatorImpl extends TaxCalculator {
     int calculateTax(Vehicle vehicle) {
          int tax;
          int co2Emission = vehicle.getCo2Emissions();
-        if(vehicle.getCo2Emissions() == 50) {
+         if(co2Emission > 1 && co2Emission <= 50) {
             tax = 10;
-        } else if ( co2Emission == 75) {
+        } else if (co2Emission > 50 && co2Emission <= 75) {
             tax = 25;
-        } else if (co2Emission == 90 ) {
+        } else if (co2Emission > 75 && co2Emission <= 90 ) {
             tax = 105;
-        } else if (co2Emission == 100) {
+        } else if (co2Emission > 90 && co2Emission <= 100) {
             tax = 125;
-        } else if (co2Emission == 110) {
+        } else if (co2Emission > 100 && co2Emission <= 110) {
             tax = 145;
-        } else if (co2Emission == 130) {
+        } else if (co2Emission > 110 && co2Emission <= 130) {
             tax = 165;
-        } else if (co2Emission == 150) {
+        } else if (co2Emission > 130 && co2Emission <= 150) {
             tax = 205;
-        } else if (co2Emission == 170) {
+        } else if (co2Emission > 150 && co2Emission <= 170) {
             tax = 515;
-        } else if (co2Emission == 190) {
+        } else if (co2Emission > 170 && co2Emission <= 190) {
             tax = 830;
-        } else if (co2Emission == 225) {
+        } else if (co2Emission > 190 && co2Emission <= 225) {
             tax = 1240;
-        } else if (co2Emission == 255) {
+        } else if (co2Emission > 225 && co2Emission <= 255) {
             tax = 1760;
-        } else if (co2Emission == 256) {
+        } else if (co2Emission >= 256) {
             tax = 2070;
         } else {
-            tax = 0;
-        }
+             tax = 0;
+         }
         return tax;
     }
 }
