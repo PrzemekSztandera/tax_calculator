bb777fd707880c969e5b889c3b3399d35b3c3abe
sztanderap <sztandera.p@gmail.com>
2020-01-20 14:29:47 +0000

    add new TaxCalculatorImpl

--- exercises/java/src/main/java/tax/TaxCalculatorImpl.java
+++ exercises/java/src/main/java/tax/TaxCalculatorImpl.java
@@ -11,7 +11,7 @@ public class TaxCalculatorImpl extends TaxCalculator {
          int tax = 0;
          int co2Emission = vehicle.getCo2Emissions();
 
-         if (co2Emission == 0) { tax = 0; }
+         if (co2Emission == 0) { return tax; }
          else if (co2Emission <= 50) { tax = 10; }
          else if(co2Emission <= 75) { tax = 25; }
          else if (co2Emission <= 90) { tax = 105; }
@@ -23,7 +23,7 @@ public class TaxCalculatorImpl extends TaxCalculator {
          else if (co2Emission <= 190) { tax = 830; }
          else if (co2Emission <= 225) { tax = 1240; }
          else if (co2Emission <= 255) { tax = 1760; }
-         else if (co2Emission > 255) { tax = 2070; }
+         else { tax = 2070; }
 
         return tax;
     }
