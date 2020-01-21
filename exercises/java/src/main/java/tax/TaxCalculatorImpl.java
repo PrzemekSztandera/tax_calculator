55415bcb473dd0a4bf85a2a7771b49918d4cc99a
sztanderap <sztandera.p@gmail.com>
2020-01-21 10:43:01 +0000

    add featureToggle for petrol cars

--- exercises/java/src/main/java/tax/TaxCalculatorImpl.java
+++ exercises/java/src/main/java/tax/TaxCalculatorImpl.java
@@ -1,9 +1,17 @@
 package tax;
 
+
 public class TaxCalculatorImpl extends TaxCalculator {
+
+    private boolean flag;
 
     public TaxCalculatorImpl(int year) {
         super(year);
+    }
+
+    public TaxCalculatorImpl(int year, boolean flag) {
+        super(year);
+        this.flag = flag;
     }
 
     @Override
@@ -11,36 +19,45 @@ public class TaxCalculatorImpl extends TaxCalculator {
          int tax = 0;
          int co2Emission = vehicle.getCo2Emissions();
 
-         if(vehicle.getFuelType().equals(FuelType.PETROL)) {
-             if (co2Emission == 0) { return tax; }
-             else if (co2Emission <= 50) { tax = 10; }
-             else if(co2Emission <= 75) { tax = 25; }
-             else if (co2Emission <= 90) { tax = 105; }
-             else if (co2Emission <= 100) { tax = 125; }
-             else if (co2Emission <= 110) { tax = 145; }
-             else if (co2Emission <= 130) { tax = 165; }
-             else if (co2Emission <= 150) { tax = 205; }
-             else if (co2Emission <= 170) { tax = 515; }
-             else if (co2Emission <= 190) { tax = 830; }
-             else if (co2Emission <= 225) { tax = 1240; }
-             else if (co2Emission <= 255) { tax = 1760; }
-             else { tax = 2070; }
-         }
-         if(vehicle.getFuelType().equals(FuelType.DIESEL)) {
-             if (co2Emission == 0) { return tax; }
-             else if (co2Emission <= 50) { tax = 25; }
-             else if(co2Emission <= 75) { tax = 105; }
-             else if (co2Emission <= 90) { tax = 125; }
-             else if (co2Emission <= 100) { tax = 145; }
-             else if (co2Emission <= 110) { tax = 165; }
-             else if (co2Emission <= 130) { tax = 205; }
-             else if (co2Emission <= 150) { tax = 515; }
-             else if (co2Emission <= 170) { tax = 830; }
-             else if (co2Emission <= 190) { tax = 1240; }
-             else if (co2Emission <= 225) { tax = 1760; }
-             else { tax = 2070; }
-         }
+        if(flag) {
+            if((this.getYear() - vehicle.getDateOfFirstRegistration().getYear()) > 1 && vehicle.getListPrice() > 40000) {
+                if(vehicle.getFuelType().equals(FuelType.PETROL)) {
+                    tax = 450;
+                }
+            }
+        } else {
+            if(vehicle.getFuelType().equals(FuelType.PETROL)) {
+                if (co2Emission == 0) { return tax; }
+                else if (co2Emission <= 50) { tax = 10; }
+                else if(co2Emission <= 75) { tax = 25; }
+                else if (co2Emission <= 90) { tax = 105; }
+                else if (co2Emission <= 100) { tax = 125; }
+                else if (co2Emission <= 110) { tax = 145; }
+                else if (co2Emission <= 130) { tax = 165; }
+                else if (co2Emission <= 150) { tax = 205; }
+                else if (co2Emission <= 170) { tax = 515; }
+                else if (co2Emission <= 190) { tax = 830; }
+                else if (co2Emission <= 225) { tax = 1240; }
+                else if (co2Emission <= 255) { tax = 1760; }
+                else { tax = 2070; }
+            }
+            if(vehicle.getFuelType().equals(FuelType.DIESEL)) {
+                if (co2Emission == 0) { return tax; }
+                else if (co2Emission <= 50) { tax = 25; }
+                else if(co2Emission <= 75) { tax = 105; }
+                else if (co2Emission <= 90) { tax = 125; }
+                else if (co2Emission <= 100) { tax = 145; }
+                else if (co2Emission <= 110) { tax = 165; }
+                else if (co2Emission <= 130) { tax = 205; }
+                else if (co2Emission <= 150) { tax = 515; }
+                else if (co2Emission <= 170) { tax = 830; }
+                else if (co2Emission <= 190) { tax = 1240; }
+                else if (co2Emission <= 225) { tax = 1760; }
+                else { tax = 2070; }
+            }
+        }
 
         return tax;
     }
+
 }
